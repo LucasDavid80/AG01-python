629a2d5f56654080404d84c15d248b05c7f453b7
Lucas David <lucasdavidoliveira80@gmail.com>
2025-06-05 18:09:16 -0300

    exercicio2.py

---
+++ exercicio2.py
@@ -0,0 +1,33 @@
+# Importa Symbol - possibilidade de operações com simbolos.
+from sympy import Derivative, Integral, Symbol, S, root
+
+# Definindo o valor de c(c=matricula % 10).
+c = (62 % 10)
+# c vai ser igual 2
+
+# Equação da velocidade
+
+
+def velocidade(t):
+    return 6 * (c + 1) * root(t, 5) - 3 * (c+1) * (t**2) + (t / (c + 3)) - 4
+
+
+t = Symbol('t')
+
+# Calcula a integral indefinida da função velocidade que é o deslocamento.
+Resultado_integral = Integral(velocidade(t), t).doit()
+
+print('\n Integral da função velocidade = função deslocamento.')
+print(Resultado_integral)
+
+# Calcula o deslocamento entre t=2 e t=5.
+Resultado_deslocamento = Integral(velocidade(t), (t, 2, 5)).doit()
+
+print('\n Deslocamento entre t=2 e t=5.')
+print(Resultado_deslocamento)
+
+# Calcula a equação da aceleração, que é a derivada da velocidade.
+
+Resultado_derivada = Derivative(velocidade(t), t).doit()
+print('\n Derivada da função velocidade = função aceleração.')
+print(Resultado_derivada)
